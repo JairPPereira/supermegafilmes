72bcf4dda3d4be8688ff7b65c561affb76256598
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2023-10-15 16:39:27 -0300

    ggg

--- script.js
+++ script.js
@@ -1,330 +1,330 @@
 var filmes = [
     {
-        "titulo": "Titulo Ewoks: A Batalha de Endor",
+        "titulo": "Ewoks: A Batalha de Endor",
         "descricao": "",
         "imdb": "tt0089110",
         "imagem": "https://image.tmdb.org/t/p/w300//nmiu6FQsFZCtBZlzcnn5vbUG5CW.jpg"
     },
     {
-        "titulo": "Titulo Morte S\u00fabita",
+        "titulo": "Morte S\u00fabita",
         "descricao": "",
         "imdb": "tt0114576",
         "imagem": "https://image.tmdb.org/t/p/w300//vBu3ggozN5xJjUzrkYgAVoI7LLI.jpg"
     },
     {
-        "titulo": "Titulo Don't Look Away",
+        "titulo": "Don't Look Away",
         "descricao": "",
         "imdb": "tt26453759",
         "imagem": "https://image.tmdb.org/t/p/w300//iiYx85daT0DzoU4XQGgaynvDtn8.jpg"
     },
     {
-        "titulo": "Titulo Walking Against the Rain",
+        "titulo": "Walking Against the Rain",
         "descricao": "",
         "imdb": "tt11859058",
         "imagem": "https://image.tmdb.org/t/p/w300//3pbFEmPjUj3w6LCO7zPWxWJ08lF.jpg"
     },
     {
-        "titulo": "Titulo La terre int\u00e9rieure",
+        "titulo": "La terre int\u00e9rieure",
         "descricao": "",
         "imdb": "tt12390218",
         "imagem": "https://image.tmdb.org/t/p/w300//wFkZZMA667hsay87asEUf0XqrMY.jpg"
     },
     {
-        "titulo": "Titulo Sirona",
+        "titulo": "Sirona",
         "descricao": "",
         "imdb": "tt14713328",
         "imagem": "https://image.tmdb.org/t/p/w300//lwarsyKCCJiXyZGYzrnqYWi9qTm.jpg"
     },
     {
-        "titulo": "Titulo Night of the Caregiver",
+        "titulo": "Night of the Caregiver",
         "descricao": "",
         "imdb": "tt15520136",
         "imagem": "https://image.tmdb.org/t/p/w300//8LGkdJpHWvUh3iAngLIVQF3pNhk.jpg"
     },
     {
-        "titulo": "Titulo O Jogo da Invoca\u00e7\u00e3o",
+        "titulo": "O Jogo da Invoca\u00e7\u00e3o",
         "descricao": "",
         "imdb": "tt10092698",
         "imagem": "https://image.tmdb.org/t/p/w300//v2DQ58q9XMruPQSUqifMCzvl586.jpg"
     },
     {
-        "titulo": "Titulo Cheia de Gra\u00e7a",
+        "titulo": "Cheia de Gra\u00e7a",
         "descricao": "",
         "imdb": "tt4184744",
         "imagem": "https://image.tmdb.org/t/p/w300//7HgcL2MsgXwzOU2OsQRr0jpF3Oa.jpg"
     },
     {
-        "titulo": "Titulo Retratos Fantasmas",
+        "titulo": "Retratos Fantasmas",
         "descricao": "",
         "imdb": "tt27502227",
         "imagem": "https://image.tmdb.org/t/p/w300//akVsEkPbb41DQ0vODFZRtzXjuyy.jpg"
     },
     {
-        "titulo": "Titulo A Porta ao Lado",
+        "titulo": "A Porta ao Lado",
         "descricao": "",
         "imdb": "tt21186266",
         "imagem": "https://image.tmdb.org/t/p/w300//8pYsfzwI20pzzwVeEIPS2Sb0zFA.jpg"
     },
     {
-        "titulo": "Titulo O Dia que Mudou o Mundo",
+        "titulo": "O Dia que Mudou o Mundo",
         "descricao": "",
         "imdb": "tt9178020",
         "imagem": "https://image.tmdb.org/t/p/w300//n1Uw2cMlQr9oz6sbekuEcCs4e9U.jpg"
     },
     {
-        "titulo": "Titulo O Motorista de Aplicativo",
+        "titulo": "O Motorista de Aplicativo",
         "descricao": "",
         "imdb": "tt11957450",
         "imagem": "https://image.tmdb.org/t/p/w300//lfcR6c3MPoScJnzqfZCdKfNZVqB.jpg"
     },
     {
-        titulo: 'Titulo O Dia do Casamento',
+        titulo: 'O Dia do Casamento',
         descricao: '',
         imdb: 'tt12869276',
         imagem: 'https://image.tmdb.org/t/p/w300//7VxK0smYw3wyqBq9vGbINV4Cvy8.jpg'
 },
 {
-        titulo: 'Titulo O Preço do Prazer',
+        titulo: 'O Preço do Prazer',
         descricao: '',
         imdb: 'tt11871074',
         imagem: 'https://image.tmdb.org/t/p/w300//uvnvE2lwwcFH4Smc4ilz194EdoD.jpg'
 },
 {
-        titulo: 'Titulo Jack Walker, Um Herói em Ação',
+        titulo: 'Jack Walker, Um Herói em Ação',
         descricao: '',
         imdb: 'tt8474040',
         imagem: 'https://image.tmdb.org/t/p/w300//4pru9cSmnHDGFio5BX9n2j2cCVb.jpg'
 },
 {
-        titulo: 'Titulo Turma Reunida no Natal',
+        titulo: 'Turma Reunida no Natal',
         descricao: '',
         imdb: 'tt18884832',
         imagem: 'https://image.tmdb.org/t/p/w300//3hGT2qtlSPMj1smIFgNftQvs7pc.jpg'
 },
 {
-        titulo: 'Titulo Um Natal Improvável',
+        titulo: 'Um Natal Improvável',
         descricao: '',
         imdb: 'tt22302170',
         imagem: 'https://image.tmdb.org/t/p/w300//i2ECy1dTvKwrmQzGeOgDFxJgCSc.jpg'
 },
 {
-        titulo: 'Titulo Awareness: A Realidade é Uma Ilusão',
+        titulo: 'Awareness: A Realidade é Uma Ilusão',
         descricao: '',
         imdb: 'tt18556326',
         imagem: 'https://image.tmdb.org/t/p/w300//msgWkL23ZslTVZJRYJIng83mYny.jpg'
 },
 {
-        titulo: 'Titulo The Loud House: Uma Verdadeira Família Assombrada',
+        titulo: 'The Loud House: Uma Verdadeira Família Assombrada',
         descricao: '',
         imdb: 'tt27428598',
         imagem: 'https://image.tmdb.org/t/p/w300//30we4NrF5F1FcBDklBGXvCDn1pG.jpg'
 },
 {
-        titulo: 'Titulo Era uma Vez uma Estrela',
+        titulo: 'Era uma Vez uma Estrela',
         descricao: '',
         imdb: 'tt22696288',
         imagem: 'https://image.tmdb.org/t/p/w300//9CELDhMAq2rbQsSqJYotWGCkeRO.jpg'
 },
 {
-        titulo: 'Titulo Instinto Policial',
+        titulo: 'Instinto Policial',
         descricao: '',
         imdb: 'tt10758146',
         imagem: 'https://image.tmdb.org/t/p/w300//ls4g4OxSCf2egNa0D548VEbGL16.jpg'
 },
 {
-        titulo: 'Titulo Angie: Garotas Perdidas',
+        titulo: 'Angie: Garotas Perdidas',
         descricao: '',
         imdb: 'tt9242768',
         imagem: 'https://image.tmdb.org/t/p/w300//wxFGebAxXTQBEyuepPB2MUm88JQ.jpg'
 },
 {
-        titulo: 'Titulo Prisoner s Daughter',
+        titulo: 'Prisoner s Daughter',
         descricao: '',
         imdb: 'tt4671326',
         imagem: 'https://image.tmdb.org/t/p/w300//2NYd7jKFnilcIOUfDI8o2TOuCmu.jpg'
 },
 {
-        titulo: 'Titulo Homens à beira de um ataque de nervos',
+        titulo: 'Homens à beira de um ataque de nervos',
         descricao: '',
         imdb: 'tt10945232',
         imagem: 'https://image.tmdb.org/t/p/w300//1xHzWFq0TLaFLWw4J6h44rZmZJ2.jpg'
 },
 {
-        titulo: 'Titulo Era Uma Vez Um Conto de Fadas',
+        titulo: 'Era Uma Vez Um Conto de Fadas',
         descricao: '',
         imdb: 'tt7813294',
         imagem: 'https://image.tmdb.org/t/p/w300//rBNX6kXx9ojBGLHTTqmC0z4QhSG.jpg'
 },
 {
-        titulo: 'Titulo A Viúva',
+        titulo: 'A Viúva',
         descricao: '',
         imdb: 'tt9584128',
         imagem: 'https://image.tmdb.org/t/p/w300//9tCiLEgKlgrJ4EWWr1DPdhFR7Sb.jpg'
 },
 {
-        titulo: 'Titulo Outra Alma',
+        titulo: 'Outra Alma',
         descricao: '',
         imdb: 'tt5165620',
         imagem: 'https://image.tmdb.org/t/p/w300//hFV6FXi8oDOfbXuBKODafOev06K.jpg'
 },
 {
-    "titulo": "Titulo Twisted Neighbor",
+    "titulo": "Twisted Neighbor",
     "descricao": "",
     "imdb": "tt28710831",
     "imagem": "https://image.tmdb.org/t/p/w300//th78OAZ14AsI63UXMZL8ZijBoAo.jpg"
 },
 {
-    "titulo": "Titulo The Way Out",
+    "titulo": "The Way Out",
     "descricao": "",
     "imdb": "tt14578410",
     "imagem": "https://image.tmdb.org/t/p/w300//742YYZZYKxKzosw9UaK8lkQAvhX.jpg"
 },
 {
-    "titulo": "Titulo Static Codes",
+    "titulo": "Static Codes",
     "descricao": "",
     "imdb": "tt20518312",
     "imagem": "https://image.tmdb.org/t/p/w300//tke2YdfPnJtehXwrxfTYTfos5SS.jpg"
 },
 {
-    "titulo": "Titulo Spellbound",
+    "titulo": "Spellbound",
     "descricao": "",
     "imdb": "tt26595612",
     "imagem": "https://image.tmdb.org/t/p/w300//dWTdAnNYeb90TkzaoQ9O1fI2BLc.jpg"
 },
 {
-    "titulo": "Titulo Polis Evo 3",
+    "titulo": "Polis Evo 3",
     "descricao": "",
     "imdb": "tt13189988",
     "imagem": "https://image.tmdb.org/t/p/w300//eHt8LAmNbuP7rhGWtGyFSLNEdla.jpg"
 },
 {
-    "titulo": "Titulo Irati",
+    "titulo": "Irati",
     "descricao": "",
     "imdb": "tt15734582",
     "imagem": "https://image.tmdb.org/t/p/w300//tuX9qBWmNEm2Mx4uEeZBl6Vntvc.jpg"
 },
 {
-    "titulo": "Titulo Hostile Forces",
+    "titulo": "Hostile Forces",
     "descricao": "",
     "imdb": "tt13646080",
     "imagem": "https://image.tmdb.org/t/p/w300//xulIkobKHZpnG6tL6s2d8wuVcoQ.jpg"
 },
 {
-    "titulo": "Titulo Happy Birthday",
+    "titulo": "Happy Birthday",
     "descricao": "",
     "imdb": "tt17053204",
     "imagem": "https://image.tmdb.org/t/p/w300//g2FWEcyD0Z6OmOSy22ySLGE9ktd.jpg"
 },
 {
-    "titulo": "Titulo God Forgives, I Don't",
+    "titulo": "God Forgives, I Don't",
     "descricao": "",
     "imdb": "tt27918682",
     "imagem": "https://image.tmdb.org/t/p/w300//u9NtlmK5rqBFkNmAKguPkL81zTQ.jpg"
 },
 {
-    "titulo": "Titulo Evinced",
+    "titulo": "Evinced",
     "descricao": "",
     "imdb": "tt15203854",
     "imagem": "https://image.tmdb.org/t/p/w300//hnDf9cH3XnLbmdyBmE029l1xC44.jpg"
 },
 {
-    "titulo": "Titulo Echo Base",
+    "titulo": "Echo Base",
     "descricao": "",
     "imdb": "tt21609312",
     "imagem": "https://image.tmdb.org/t/p/w300//dbZmD7x7mc1WniATkRUMIuiV1Xq.jpg"
 },
 {
-    "titulo": "Titulo Blind Waters",
+    "titulo": "Blind Waters",
     "descricao": "",
     "imdb": "tt28138183",
     "imagem": "https://image.tmdb.org/t/p/w300//asG1vxzBWed6vOwrHq1USW7HETT.jpg"
 },
 {
-    "titulo": "Titulo Below Deck Deceit",
+    "titulo": "Below Deck Deceit",
     "descricao": "",
     "imdb": "tt28561235",
     "imagem": "https://image.tmdb.org/t/p/w300//9cWGf8ui3W0pFebimPAXiuw7E2Z.jpg"
 },
 {
-    "titulo": "Titulo C\u00edrculo de Espi\u00f5es",
+    "titulo": "C\u00edrculo de Espi\u00f5es",
     "descricao": "",
     "imdb": "tt15433600",
     "imagem": "https://image.tmdb.org/t/p/w300//jGUcSkXhBpgqIjSW5f5QKAmi2SQ.jpg"
 },
 {
-    "titulo": "Titulo Dezesseis Facadas",
+    "titulo": "Dezesseis Facadas",
     "descricao": "",
     "imdb": "tt11426232",
     "imagem": "https://image.tmdb.org/t/p/w300//o5tWAiHfxEvu2z1qkJKU4gW1uPY.jpg"
 },
 {
-    "titulo": "Titulo O Rei dos Assassinos",
+    "titulo": "O Rei dos Assassinos",
     "descricao": "",
     "imdb": "tt14057604",
     "imagem": "https://image.tmdb.org/t/p/w300//tHA2wltMBWuaTzOzSf7P2bzhmIy.jpg"
 },
 {
-    "titulo": "Titulo Angela",
+    "titulo": "Angela",
     "descricao": "",
     "imdb": "tt28118695",
     "imagem": "https://image.tmdb.org/t/p/w300//ovIX8JQSh58GKhYlhIhJohVqEaj.jpg"
 },
 {
-    "titulo": "Titulo Cemit\u00e9rio Maldito: A Origem",
+    "titulo": "Cemit\u00e9rio Maldito: A Origem",
     "descricao": "",
     "imdb": "tt14145436",
     "imagem": "https://image.tmdb.org/t/p/w300//cTtrFUxMKUX4ZGfMfydym2AZQba.jpg"
 },
 {
-    "titulo": "Titulo Mate-me se Puder",
+    "titulo": "Mate-me se Puder",
     "descricao": "",
     "imdb": "tt13022120",
     "imagem": "https://image.tmdb.org/t/p/w300//e2ghk75hCIu7N2Xk7baIJtvwMPP.jpg"
 },
 {
-    "titulo": "Titulo A Bailarina",
+    "titulo": "A Bailarina",
     "descricao": "",
     "imdb": "tt26350277",
     "imagem": "https://image.tmdb.org/t/p/w300//zrL3C3YPiuFchLgQVYSlYZyx15G.jpg"
 },
 {
-    "titulo": "Titulo Jogo Justo",
+    "titulo": "Jogo Justo",
     "descricao": "",
     "imdb": "tt16304446",
     "imagem": "https://image.tmdb.org/t/p/w300//AteINDGu2VqyI1M8mBZXYMA7Yhs.jpg"
 },
 {
-    "titulo": "Titulo A Arte da Sobreviv\u00eancia",
+    "titulo": "A Arte da Sobreviv\u00eancia",
     "descricao": "",
     "imdb": "tt15053106",
     "imagem": "https://image.tmdb.org/t/p/w300//lxRlS1EN0ByrhqkisV41l0havin.jpg"
 },
 {
-    "titulo": "Titulo Uma Bela Manh\u00e3",
+    "titulo": "Uma Bela Manh\u00e3",
     "descricao": "",
     "imdb": "tt13482828",
     "imagem": "https://image.tmdb.org/t/p/w300//f0zZGfxM6GOuOuZY61m0JcfGi4S.jpg"
 },
 {
-    "titulo": "Titulo O Exorcista: O Devoto",
+    "titulo": "O Exorcista: O Devoto",
     "descricao": "",
     "imdb": "tt12921446",
     "imagem": "https://image.tmdb.org/t/p/w300//l6nMIFfpkL5cQz4ulSV0llwOWoh.jpg"
 },
 {
-    "titulo": "Titulo Mans\u00e3o Mal-Assombrada",
+    "titulo": "Mans\u00e3o Mal-Assombrada",
     "descricao": "",
     "imdb": "tt1695843",
     "imagem": "https://image.tmdb.org/t/p/w300//3puybXuYPlZPRplIVSrExZnWibb.jpg"
 },
 {
-    "titulo": "Titulo O Monstro que vive em Mim",
+    "titulo": "O Monstro que vive em Mim",
     "descricao": "",
     "imdb": "tt17202164",
     "imagem": "https://image.tmdb.org/t/p/w300//fDBAZRH4YiqtOrcTwmwbjz4KsXe.jpg"
 },
 {
-    "titulo": "Titulo Raiva",
+    "titulo": "Raiva",
     "descricao": "",
     "imdb": "tt28492037",
     "imagem": "https://image.tmdb.org/t/p/w300//1xIvYJ6HcmD5lcXlaOhChPxLLcY.jpg"
@@ -343,7 +343,7 @@ function adicionarFilmesNaPagina(pagina) {
         listaHTML += '<a onclick="openMovie(\'' + filme.descricao + '\', \'https://superflixapi.top/filme/' + filme.imdb + '\')" class="full-click"></a>';
         listaHTML += '<div class="media-thumb" style="background-image: url(' + filme.imagem + ');"></div>';
         listaHTML += '<a onclick="openMovie(\'' + filme.descricao + '\', \'https://superflixapi.top/filme/' + filme.imdb + '\')" class="media-play"><i class="icofont-ui-play"></i></a>';
-        listaHTML += '<div class="media-info"><h5 class="media-box-title">' + filme.titulo + '</h5></div></div></div>';
+        listaHTML += '<div class="media-info"><h5 class="media-box-title">' + filme.titulo+ '</h5></div></div></div>';
     }
 
 
