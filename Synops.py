8780823e7b90cb245a244fe2270deb4544a1c464
JPP <86321461+JairPPereira@users.noreply.github.com>
2024-02-11 18:35:59 -0300

    jjj

--- Synops.py
+++ Synops.py
@@ -18,7 +18,7 @@ def get_movie_info(tmdb_id):
     else:
         return None
 
-tmdb_ids = [386732, 1227136, 1048814,]
+tmdb_ids = [805, 1230085,]
 
 # Imprime os itens no formato desejado
 for tmdb_id in tmdb_ids:
